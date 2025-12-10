94667fce8f003bf21abe3a8c244f3a833795e7f6
atrubitsin <atrubitsin@promopult.ru>
2025-12-11 05:08:27 +0700

    Update archetype_init function signature to accept component array and count

--- src/include/ecs/archetype/arhetype_init.h
+++ src/include/ecs/archetype/arhetype_init.h
@@ -4,4 +4,4 @@
 #include "../../context.h"
 #include "../ecs.h"
 
-uint32_t archetype_init(Context *ctx, ComponentMask component_mask);
+uint32_t archetype_init(Context *ctx, const uint32_t *components, size_t component_count);
