f2a03500d92e7626e5524c8d10811b86059ebafc
atrubitsin <atrubitsin@promopult.ru>
2025-12-12 06:25:10 +0700

    Refactor get_entity_by_id function to simplify entity retrieval by removing unnecessary variable

--- src/include/ecs/query/query.c
+++ src/include/ecs/query/query.c
@@ -1,9 +1,7 @@
 #include "query.h"
 
 Entity* get_entity_by_id(const Context *ctx, const EntityId entity_id) {
-    Ecs* ecs = &ctx->ecs;
-
-    Entity* entity = &ecs->entities[entity_id.index];
+    Entity* entity = &ctx->ecs.entities[entity_id.index];
     if (entity->generation != entity_id.generation) {
         return NULL;
     }
